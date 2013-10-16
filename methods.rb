3435cc7ea52535ee5a68833c163294aa7101f860
Julian <japaquin@hotmail.com>
2013-10-16 16:10:11 -0600

    added two methods to methods.rb

--- methods.rb
+++ methods.rb
@@ -1,5 +1,23 @@
-def welcome
-
-	puts "Hello World"
-
+def welcome (name = "World")
+	puts "Hello " + name
+	puts "Hello #{name}"
+	return "test"
 end
+
+def return_two_arguments (num1 = 10, num2 = 10)
+	add = num1 + num2
+	sub = num1 - num2
+	return [add,sub]  # return an array of the two results
+end
+
+welcome("Julian")
+
+welcome
+
+array = return_two_arguments( 6, 2 )
+puts array[0]
+puts array[1]
+
+add, sub = return_two_arguments( 10, 4 )
+puts add
+puts sub
